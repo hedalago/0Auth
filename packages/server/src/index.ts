abbdde5677d65733a20a78eb3f37f7e1bea7a963
HyeockJinKim <kherootz@gmail.com>
2020-07-29 00:40:07 +0900

    Add function skeleton
    Closes #12

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,35 @@
-export default {};
+import {KeyType, Property, Signature} from "../../message/src/index";
+
+type SecretKey = {
+  type: KeyType;
+  key: string;
+}
+
+type PublicKey = {
+  type: KeyType;
+  key: string;
+}
+
+// TODO: @ts-ignore should be removed, when below function is implemented.
+// @ts-ignore
+export function authPrivacy(properties: Property[], secret: SecretKey): Signature {
+  // TODO: Not implemented yet.
+}
+
+// TODO: @ts-ignore should be removed, when below function is implemented.
+// @ts-ignore
+export function authPackage(properties: Property[], secret: SecretKey): Signature {
+  // TODO: Not implemented yet.
+}
+
+// TODO: @ts-ignore should be removed, when below function is implemented.
+// @ts-ignore
+export function verifyPrivacy(properties: Property[], sign: Signature, publicKey: PublicKey): boolean {
+  // TODO: Not implemented yet.
+}
+
+// TODO: @ts-ignore should be removed, when below function is implemented.
+// @ts-ignore
+export function verifyPackage(properties: Property[], sign: Signature, publicKey: PublicKey): boolean {
+  // TODO: Not implemented yet.
+}
