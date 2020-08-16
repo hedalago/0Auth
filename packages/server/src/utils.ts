15c8f881228fa15affd9db15cebddc68fa5a42eb
HyeockJinKim <kherootz@gmail.com>
2020-08-16 17:33:25 +0900

    server: Add authentication using package mode
    Implement authentication and verification functions to check values
    used together at once.
    
    Closes #16

--- packages/server/src/utils.ts
+++ packages/server/src/utils.ts
@@ -13,7 +13,7 @@ export function hash(msg: string): string {
 }
 
 export function hashProperty(property: Property): string {
-  if (property.type == PropertyType.Hash) return property.value;
+  if (property.type === PropertyType.Hash) return property.value;
 
   return hash(`${utf8ToBase64(property.key)}:${utf8ToBase64(property.value)}`);
 }
